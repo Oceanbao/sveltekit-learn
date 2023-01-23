18c0f2373462fe472bb237f3d1a0b5e39a2aac4a
oceanbao <oceanbao@outlook.com>
2023-01-23 19:53:57 +0800

    update: add tailwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,28 @@
-<nav>
-	<a href="/">home</a>
-	<a href="/about">about</a>
-	<a href="/expected">page with expected error</a>
-	<a href="/unexpected">page with unexpected error</a>
-	<a href="/learn">learning svelte</a>
-	<a href="/spring">spring motion</a>
-	<a href="/optionalparam">optional param</a>
-	<a href="/matchparam">param matcher</a>
-	<a href="/grouproute">group route</a>
-	<a href="/resetlayout">reset layout</a>
-	<a href="/advancedloading">advanced loading</a>
-	<a href="/invalidateloader">invalidate loader</a>
-</nav>
+<script>
+	import '@app/app.css';
+</script>
+
+<header>
+	<nav>
+		<a href="/" class="text-3xl font-bold underline">home</a>
+		<a href="/about" class="text-3xl font-bold underline">about</a>
+		<a href="/expected" class="text-3xl font-bold underline">page with expected error</a>
+		<a href="/unexpected" class="text-3xl font-bold underline">page with unexpected error</a>
+		<a href="/learn" class="text-3xl font-bold underline">learning svelte</a>
+		<a href="/spring" class="text-3xl font-bold underline">spring motion</a>
+		<a href="/optionalparam" class="text-3xl font-bold underline">optional param</a>
+		<a href="/matchparam" class="text-3xl font-bold underline">param matcher</a>
+		<a href="/grouproute" class="text-3xl font-bold underline">group route</a>
+		<a href="/resetlayout" class="text-3xl font-bold underline">reset layout</a>
+		<a href="/advancedloading" class="text-3xl font-bold underline">advanced loading</a>
+		<a href="/invalidateloader" class="text-3xl font-bold underline">invalidate loader</a>
+	</nav>
+</header>
 
 <slot />
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.800);
+	}
+</style>
